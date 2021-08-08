a04902626d3a06c6dabc8471bc64b5c6855381db
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-08 22:01:58 +0300

    feat: add new standart methods for table and cells. Practice for this in the table for output parameters in the cells

--- EmojiReader/EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiReader/EmojiTableViewController.swift
@@ -17,29 +17,34 @@ class EmojiTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
+        //register of my custom cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the mine custom cell
+        cell.textLabel?.text  = "\(indexPath)"
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
