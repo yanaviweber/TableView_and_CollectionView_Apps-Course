f56028f7c3eb0b67d458487fd7934c033197e297
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-10 23:15:15 +0300

    feat: add new functionality for save data in the fields after click "Save" button

--- EmojiReader/EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/EmojiReader/NewEmojiTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
 
-    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
     
     @IBOutlet weak var emojiTextField: UITextField!
     
@@ -39,6 +39,16 @@ class NewEmojiTableViewController: UITableViewController {
     @IBAction func textChanged(_ sender: UITextField) {
         
         updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
     }
     
 }
