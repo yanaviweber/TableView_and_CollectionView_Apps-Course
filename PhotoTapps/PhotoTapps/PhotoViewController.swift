bc7d31e4545c75cc688178bc89d5441e36d0130e
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-08-12 06:22:38 +0300

    feat: add new functionality for share and save image to the second screen; success test it in the simulator

--- PhotoTapps/PhotoTapps/PhotoViewController.swift
+++ PhotoTapps/PhotoTapps/PhotoViewController.swift
@@ -23,6 +23,17 @@ class PhotoViewController: UIViewController {
     }
     
     @IBAction func shareAction(_ sender: Any) {
+        
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = {_, bool, _, _ in
+            if bool {
+                print("Успешно!")
+            }
+        }
+        
+            present(shareController, animated: true, completion: nil)
+            
     }
     
 
